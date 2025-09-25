8b3719597b82c3f9e3f67a77abbedb7bec57bf5a
James Clare <104501016+jamclar@users.noreply.github.com>
2025-09-25 17:16:09 +0100

    Add argo mount/volumes

--- noc/dags/argo.py
+++ noc/dags/argo.py
@@ -97,7 +97,7 @@ with DAG(
         env_vars=environment.DEFAULT + environment.DECODER,
         container_security_context=security.DEVARGO,
         volumes=[volumes.ARGO] + volumes.MODULES,
-        volume_mounts=mounts.MODULES
+        volume_mounts=[mounts.ARGO] + mounts.MODULES
     )
 
     list_argo >> list_devargo >> write_amrit >> read_amrit
