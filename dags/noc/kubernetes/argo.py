ea6e3294bb1d1d85aea23728b29a23718d51571c
Matthew Cazaly <matcaz@noc.ac.uk>
2025-09-25 08:54:13 +0100

    WIP state

--- dags/noc/kubernetes/argo.py
+++ dags/noc/kubernetes/argo.py
@@ -1,6 +1,9 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from kubernetes.client.models import V1VolumeMount, V1Volume
+
+
 
 
 with DAG(
@@ -22,6 +25,33 @@ with DAG(
         cmds=["bash", "-cx"],
         arguments=["echo Goodbye World"],
         task_id="pod_goodbye"
+    )
+    list_files = KubernetesPodOperator(
+        name="list-files",
+        task_id="list_files",
+        image="debian",
+        cmds=["bash", "-cx"],
+        arguments=["ls -lsarth /users/devargo"],
+        security_context={
+            "privileged": False,
+            "capabilities": {
+                "drop": ["ALL"]
+            }
+        },
+        volumes=[
+            V1Volume(
+                name="devargo",
+                persistent_volume_claim="nocl-scale-bodc2-users-devargo-airflow",
+                read_only=True
+            )
+        ],
+        volume_mounts=[
+            V1VolumeMount(
+                name="devargo",
+                mount_path="/users/devargo",
+                read_only=True
+            )
+        ]
     )
 
     say_hello >> say_goodbye
