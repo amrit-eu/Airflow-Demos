c5158b1490b466ae9bddacff353a253e79fe79e2
James Clare <104501016+jamclar@users.noreply.github.com>
2025-09-26 12:41:26 +0100

    Update images.py

--- noc/dags/util/images.py
+++ noc/dags/util/images.py
@@ -2,4 +2,4 @@ from typing import Final
 
 __all__ = ["DECODER"]
 
-DECODER: Final[str] = "docker-repo.bodc.me/jamclar/amrit/decoder_matlab_tool_v2:latest"
+DECODER: Final[str] = "docker-repo.bodc.me/jamclar/amrit/decoder_matlab_tool_v4:latest"
